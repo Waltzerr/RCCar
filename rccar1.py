9886c203ee3c5d8ef776712eda2d71c3d21592d3
Waltzerr <86032176+Waltzerr@users.noreply.github.com>
2021-06-17 13:58:17 +1000

    Update rccar1.py

--- rccar1.py
+++ rccar1.py
@@ -1,6 +1,4 @@
 import RPi.GPIO as GPIO
-import pynput
-from pynput.keyboard import Listener
 from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
@@ -9,23 +7,15 @@ GPIO.setup(11, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(16, GPIO.OUT)
 GPIO.setup(18, GPIO.OUT)
-  
-def on_press(key):
-    print("Key pressed: {0}".format(key))
 
-def on_release(key):
-    print("Key released: {0}".format(key))
-    
-with Listener(on_press=on_press, on_release=on_release) as listener:  # Create an instance of Listener
-    listener.join()  # Join the listener thread to the main thread to keep waiting for keys
-
-#GPIO.output(18, GPIO.HIGH) #backwards
+GPIO.output(18, GPIO.HIGH) #backwards
 #sleep(2)
-#GPIO.output(18, GPIO.LOW)
 
-#GPIO.output(13, GPIO.HIGH) #right
-#sleep(2)
-#GPIO.output(13, GPIO.LOW)
+
+GPIO.output(13, GPIO.HIGH) #right
+sleep(2)
+GPIO.output(13, GPIO.LOW)
+GPIO.output(18, GPIO.LOW)
 
 #GPIO.output(16, GPIO.HIGH) #forwards
 #sleep(2)
