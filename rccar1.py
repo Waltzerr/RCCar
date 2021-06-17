13fa92677b251758f30af3c753edf31fd98ede33
Waltzerr <86032176+Waltzerr@users.noreply.github.com>
2021-06-17 13:59:30 +1000

    Update rccar1.py

--- rccar1.py
+++ rccar1.py
@@ -8,11 +8,11 @@ GPIO.setup(13, GPIO.OUT)
 GPIO.setup(16, GPIO.OUT)
 GPIO.setup(18, GPIO.OUT)
 
-GPIO.output(18, GPIO.HIGH) #backwards
+GPIO.output(13, GPIO.HIGH) #backwards
 #sleep(2)
 
 
-GPIO.output(13, GPIO.HIGH) #right
+GPIO.output(18, GPIO.HIGH) #right
 sleep(2)
 GPIO.output(13, GPIO.LOW)
 GPIO.output(18, GPIO.LOW)
