576c34d3aedb23a320c60b58d01df5d8e309fdcd
Waltzerr <86032176+Waltzerr@users.noreply.github.com>
2021-06-17 13:33:45 +1000

    Update rccar1.py

--- rccar1.py
+++ rccar1.py
@@ -9,13 +9,11 @@ GPIO.setup(13, GPIO.OUT)
 GPIO.setup(16, GPIO.OUT)
 GPIO.setup(18, GPIO.OUT)
 
-while True:
+while readchar.readkey():
   input=readchar.readkey()
-  print(input)
   GPIO.output(16, GPIO.LOW)
   if input=="w":
     GPIO.output(16, GPIO.HIGH) #forwards
-    print("going forward")
     
   if input=="s":
     GPIO.output(18, GPIO.HIGH) #backwards
