99379e0982bf7d0c2febf3f015c5c611e01b4189
Waltzerr <86032176+Waltzerr@users.noreply.github.com>
2021-06-17 12:42:11 +1000

    Forward with w

--- rccar1.py
+++ rccar1.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import keyboard
 from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
@@ -8,20 +9,30 @@ GPIO.setup(13, GPIO.OUT)
 GPIO.setup(16, GPIO.OUT)
 GPIO.setup(18, GPIO.OUT)
 
-GPIO.output(18, GPIO.HIGH)
-sleep(2)
-GPIO.output(18, GPIO.LOW)
+while True:
+  try:
+    if keyboard.is_pressed('w'):
+            print("Going forward!")
+            GPIO.output(16, GPIO.HIGH)
+    else:
+      GPIO.output(16, GPIO.LOW)
+  except:
+    break
 
-GPIO.output(13, GPIO.HIGH)
-sleep(2)
-GPIO.output(13, GPIO.LOW)
+#GPIO.output(18, GPIO.HIGH) #backwards
+#sleep(2)
+#GPIO.output(18, GPIO.LOW)
 
-GPIO.output(16, GPIO.HIGH)
-sleep(2)
-GPIO.output(16, GPIO.LOW)
+#GPIO.output(13, GPIO.HIGH) #right
+#sleep(2)
+#GPIO.output(13, GPIO.LOW)
 
-GPIO.output(11, GPIO.HIGH)
-sleep(2)
-GPIO.output(11, GPIO.LOW)
+#GPIO.output(16, GPIO.HIGH) #forwards
+#sleep(2)
+#GPIO.output(16, GPIO.LOW)
+
+#GPIO.output(11, GPIO.HIGH) #left
+#sleep(2)
+#GPIO.output(11, GPIO.LOW)
 
 GPIO.cleanup()
