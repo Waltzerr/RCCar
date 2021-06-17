c770b20396773743332c3f89ad67f01760157388
Waltzerr <86032176+Waltzerr@users.noreply.github.com>
2021-06-17 14:05:48 +1000

    control with wasd

--- rccar1.py
+++ rccar1.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import readchar
 from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
@@ -8,14 +9,39 @@ GPIO.setup(13, GPIO.OUT)
 GPIO.setup(16, GPIO.OUT)
 GPIO.setup(18, GPIO.OUT)
 
-GPIO.output(13, GPIO.HIGH) #backwards
-sleep(2)
+while True:
+  input=readchar.readkey()
+  if input="w":
+    GPIO.output(16, GPIO.HIGH) #forwards
+  else:
+    GPIO.output(16, GPIO.LOW) #forwards
+    
+  if input="s":
+    GPIO.output(18, GPIO.HIGH) #backwards
+  else:
+    GPIO.output(18, GPIO.LOW) #backwards
+    
+  if input="a":
+    GPIO.output(11, GPIO.HIGH) #left
+  else:
+    GPIO.output(11, GPIO.LOW) #left
+    
+  if input="d":
+    GPIO.output(13, GPIO.HIGH) #right
+  else:
+    GPIO.output(13, GPIO.LOW) #right
+    
+  if input="q":
+    break
+  
 
+#GPIO.output(18, GPIO.HIGH) #backwards
+#sleep(2)
+#GPIO.output(18, GPIO.LOW)
 
-GPIO.output(18, GPIO.HIGH) #right
-sleep(2)
-GPIO.output(13, GPIO.LOW)
-GPIO.output(18, GPIO.LOW)
+#GPIO.output(13, GPIO.HIGH) #right
+#sleep(2)
+#GPIO.output(13, GPIO.LOW)
 
 #GPIO.output(16, GPIO.HIGH) #forwards
 #sleep(2)
