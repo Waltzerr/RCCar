f53203ef9376b2dffee3af5d609d7c4f0fc0657c
Waltzer <102108760@student.swin.edu.au>
2021-06-21 12:44:31 +1000

    test

--- controller.py
+++ controller.py
@@ -6,6 +6,9 @@ class MyController(Controller):
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
 
+    def on_L3_left(self):
+       print("cum")
+
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 controller.listen()
