04cc2b34568abb739c6c5f742cc5ba53d6b8f0e6
Waltzer <102108760@student.swin.edu.au>
2021-06-22 14:09:41 +1000

    testing servos

---
+++ turret.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(36, GPIO.OUT)
+servo1 = GPIO.PWM(36,50)
+
+servo1.start(0)
+sleep(2)
+
+duty = 2
+
+while duty <= 12:
+    servo1.ChangeDutyCycle(duty)
+    sleep(1)
+    duty += 1
+
+sleep(2)
+
+servo1.ChangeDutyCycle(7)
+sleep(2)
+
+servo1.ChangeDutyCycle(2)
+sleep(0.5)
+servo1.ChangeDutyCycle(0)
+
+servo1.stop()
+GPIO.cleanup()
