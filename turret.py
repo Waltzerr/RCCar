4d0cd0d95ab5490ecd3ef44c11164e6dcc33c82e
Waltzer <102108760@student.swin.edu.au>
2021-06-22 14:43:02 +1000

    servo with controller 1

--- turret.py
+++ turret.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 from time import sleep
+from pyPS4Controller.controller import Controller
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -7,17 +8,47 @@ GPIO.setup(36, GPIO.OUT)
 servo1 = GPIO.PWM(36,50)
 
 servo1.start(0)
+servo1.ChangeDutyCycle(0)
 
-try:
-    while True:
-        angle = float(input('Enter angle between 0 and 180: '))
-        servo1.ChangeDutyCycle(2+(angle/18))
-        sleep(0.5)
+duty = 0
+
+class MyController(Controller):
+    def __init__(self, **kwargs):
+        Controller.__init__(self, **kwargs)
+
+    def on_L3_left(self, value):
+        global duty
+        duty += 1
+        servo1.ChangeDutyCycle(duty)
+        sleep(0.1)
         servo1.ChangeDutyCycle(0)
 
-finally:
-    servo1.stop()
-    GPIO.cleanup()
+    def on_L3_right(self, value):
+        global duty
+        duty += -1
+        servo1.ChangeDutyCycle(duty)
+        sleep(0.1)
+        servo1.ChangeDutyCycle(0)
+
+try:
+    controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
+    # you can start listening before controller is paired, as long as you pair it within the timeout window
+    controller.listen(timeout=60)
+except:
+    print("Error connecting to controller")
+    quit()
+
+
+# try:
+#     while True:
+#         angle = float(input('Enter angle between 0 and 180: '))
+#         servo1.ChangeDutyCycle(2+(angle/18))
+#         sleep(0.5)
+#         servo1.ChangeDutyCycle(0)
+
+# finally:
+#     servo1.stop()
+#     GPIO.cleanup()
 
 # sleep(2)
 
