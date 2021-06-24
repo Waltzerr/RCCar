7bd19f485cc1598d492ab76c630b20584a15595e
Waltzer <102108760@student.swin.edu.au>
2021-06-24 11:25:28 +1000

    and bruh moment

--- turret.py
+++ turret.py
@@ -19,12 +19,12 @@ class MyController(Controller):
 
     def on_L3_left(self, value):
         global xState
-        if -value < (32767/2) & xState != 1:
+        if -value < (32767/2) and xState != 1:
             servo1.ChangeDutyCycle(10)
             sleep(0.3)
             servo1.ChangeDutyCycle(0)
             xState=1
-        if -value > (32767/2) & xState != 2:
+        if -value > (32767/2) and xState != 2:
             servo1.ChangeDutyCycle(12)
             sleep(0.3)
             servo1.ChangeDutyCycle(0)
