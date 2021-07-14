f892a31e2b6f3f19b3ec1a1d736e7cda9bd5b252
Waltzer <102108760@student.swin.edu.au>
2021-07-14 13:24:08 +1000

    turret 1 back to 5 angles cause servos are ass

--- servoController.py
+++
@@ -1,7 +0,0 @@
-import turret2
-import RPi.GPIO as GPIO
-from time import sleep
-
-print("cum1")
-if turret2.controller.on_R2_press:
-    print('cum')
--- turret.py
+++ turret.py
@@ -6,95 +6,58 @@ GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(36, GPIO.OUT)
 servo1 = GPIO.PWM(36,50)
-GPIO.setup(32, GPIO.OUT)
-servo2 = GPIO.PWM(32,50)
-GPIO.setup(29, GPIO.OUT)
 
 servo1.start(0)
 servo1.ChangeDutyCycle(2)
 servo1.ChangeDutyCycle(0)
-servo2.start(0)
-servo2.ChangeDutyCycle(2)
-servo2.ChangeDutyCycle(0)
 
 xState = 0
-yState = 0
-maxValue = 32767
-
-def getState(value):
-        global maxValue
-        for div in range(1,6):
-            if value <= div*(maxValue/5):
-                return div
 
 class MyController(Controller):
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
 
-    def on_R3_left(self, value):
+    def on_L3_left(self, value):
         global xState
-        state = getState(-value)
-        if -state != xState:
-            print(f"{state}, {xState}")
-            servo1.ChangeDutyCycle(state+7)
-            sleep(0.05)
+        if -value <= (15384) and xState != -1:
+            servo1.ChangeDutyCycle(9.5)
+            sleep(0.1)
             servo1.ChangeDutyCycle(0)
-            xState=-state
+            xState=-1
+        if -value > (17384) and xState != -2:
+            servo1.ChangeDutyCycle(12)
+            sleep(0.1)
+            servo1.ChangeDutyCycle(0)
+            xState=-2
         print(f"Value: {value}, State: {xState}")
         
 
-    def on_R3_right(self, value):
+    def on_L3_right(self, value):
         global xState
-        state = getState(value)
-        if state != xState:
-            print(f"{state}, {xState}")
-            servo1.ChangeDutyCycle(7-state)
-            sleep(0.05)
+        if value <= (15384) and xState != 1:
+            servo1.ChangeDutyCycle(4.5)
+            sleep(0.1)
             servo1.ChangeDutyCycle(0)
-            xState=state
-        print(f"Value: {value}, State: {xState}")
+            xState=1
+        if value > (17384) and xState != 2:
+            servo1.ChangeDutyCycle(2)
+            sleep(0.1)
+            servo1.ChangeDutyCycle(0)
+            xState=2
 
-    def on_R3_x_at_rest(self):
+    def on_L3_x_at_rest(self):
         global xState
         servo1.ChangeDutyCycle(7)
-        sleep(0.2)
+        sleep(0.1)
         servo1.ChangeDutyCycle(0)
         xState = 0
 
-    def on_R3_up(self, value):
-        global yState
-        state = getState(-value)
-        if -state != yState:
-            print(f"{state}, {yState}")
-            servo2.ChangeDutyCycle(7-state)
-            sleep(0.05)
-            servo2.ChangeDutyCycle(0)
-            yState=-state
-        print(f"Value: {value}, State: {yState}")
-        
-
-    def on_R3_down(self, value):
-        global yState
-        state = getState(value)
-        if state != yState:
-            print(f"{state}, {yState}")
-            servo2.ChangeDutyCycle(state+7)
-            sleep(0.05)
-            servo2.ChangeDutyCycle(0)
-            yState=state
-        print(f"Value: {value}, State: {yState}")
-
-    def on_R3_y_at_rest(self):
-        global yState
-        servo2.ChangeDutyCycle(7)
-        sleep(0.2)
-        servo2.ChangeDutyCycle(0)
-        yState = 0
-
     def on_circle_press(self):
         servo1.ChangeDutyCycle(7)
-        sleep(0.25)
+        sleep(0.5)
         servo1.ChangeDutyCycle(0)
+        servo1.stop()
+        GPIO.cleanup()
         quit()
 
     def on_R2_press(self, value):
@@ -107,7 +70,7 @@ try:
     controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
     controller.listen(timeout=60)
 except:
+    print("Error")
     servo1.stop()
     GPIO.cleanup()
-    print("Turret disconnected")
     quit()
