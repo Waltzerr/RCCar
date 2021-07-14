8e3eadd1038d663fb3c8aadd7ebbd4b08657a6a7
Waltzer <102108760@student.swin.edu.au>
2021-07-14 14:38:05 +1000

    git fucked itself

--- rccarController.py
+++ rccarController.py
@@ -143,7 +143,6 @@ class MyController(Controller):
 
 try:
     controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
-    # you can start listening before controller is paired, as long as you pair it within the timeout window
     controller.listen(timeout=60)
 except:
    servo1.ChangeDutyCycle(0)
