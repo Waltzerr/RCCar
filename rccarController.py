db50370159bbca97f3c2a38751a7429a60f91a52
Waltzer <102108760@student.swin.edu.au>
2021-07-14 15:28:07 +1000

    bitch

--- rccarController.py
+++ rccarController.py
@@ -25,10 +25,6 @@ sleep(0.2)
 servo1.ChangeDutyCycle(0)
 servo2.ChangeDutyCycle(0)
 
-
-
-servo2.ChangeDutyCycle(0)
-
 xState = 0
 yState = 0
 
@@ -67,13 +63,13 @@ class MyController(Controller):
             sleep(0.1)
             servo1.ChangeDutyCycle(0)
             xState=-1
-            print(f"Value: {value}, X: {xState}")
+            #print(f"Value: {value}, X: {xState}")
       elif -value > (17384) and xState != -2:
             servo1.ChangeDutyCycle(12)
             sleep(0.1)
             servo1.ChangeDutyCycle(0)
             xState=-2
-            print(f"Value: {value}, X: {xState}")
+            #print(f"Value: {value}, X: {xState}")
       
 
    def on_R3_right(self, value):
@@ -83,13 +79,13 @@ class MyController(Controller):
             sleep(0.1)
             servo1.ChangeDutyCycle(0)
             xState=1
-            print(f"Value: {value}, X: {xState}")
+            #print(f"Value: {value}, X: {xState}")
       elif value > (17384) and xState != 2:
             servo1.ChangeDutyCycle(2)
             sleep(0.1)
             servo1.ChangeDutyCycle(0)
             xState=2
-            print(f"Value: {value}, X: {xState}")
+            #print(f"Value: {value}, X: {xState}")
 
    def on_R3_x_at_rest(self):
       global xState
@@ -98,7 +94,7 @@ class MyController(Controller):
             sleep(0.1)
             servo1.ChangeDutyCycle(0)
             xState = 0
-            print(f"X: {xState}")
+            #print(f"X: {xState}")
 
    def on_R3_down(self, value):
       global yState
@@ -107,13 +103,13 @@ class MyController(Controller):
             sleep(0.1)
             servo2.ChangeDutyCycle(0)
             yState=-1
-            print(f"Value: {value}, Y: {yState}")
+            #print(f"Value: {value}, Y: {yState}")
       elif value > (17384) and yState != -2:
             servo2.ChangeDutyCycle(12)
             sleep(0.1)
             servo2.ChangeDutyCycle(0)
             yState=-2
-            print(f"Value: {value}, Y: {yState}")
+            #print(f"Value: {value}, Y: {yState}")
       
 
    def on_R3_up(self, value):
@@ -123,13 +119,13 @@ class MyController(Controller):
             sleep(0.1)
             servo2.ChangeDutyCycle(0)
             yState=1
-            print(f"Value: {value}, Y: {yState}")
+            #print(f"Value: {value}, Y: {yState}")
       elif -value > (17384) and yState != 2:
             servo2.ChangeDutyCycle(2)
             sleep(0.1)
             servo2.ChangeDutyCycle(0)
             yState=2
-            print(f"Value: {value}, Y: {yState}")
+            #print(f"Value: {value}, Y: {yState}")
 
    def on_R3_y_at_rest(self):
       global yState
@@ -138,7 +134,7 @@ class MyController(Controller):
             sleep(0.1)
             servo2.ChangeDutyCycle(0)
             yState = 0
-            print(f"Y: {yState}")
+            #print(f"Y: {yState}")
 
    def on_R2_press(self, value):
       GPIO.output(29, GPIO.HIGH)
