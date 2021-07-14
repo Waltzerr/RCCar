945cc480d21968f80efd83c360d02c4beed70ca5
Waltzer <102108760@student.swin.edu.au>
2021-07-14 14:46:06 +1000

    x press was missing for some reason

--- rccarController.py
+++ rccarController.py
@@ -44,6 +44,9 @@ class MyController(Controller):
    def on_x_release(self):
       GPIO.output(16, GPIO.LOW)
 
+   def on_x_press(self):
+      GPIO.output(16, GPIO.HIGH)
+
    def on_circle_release(self):
       GPIO.output(18, GPIO.LOW)
 
